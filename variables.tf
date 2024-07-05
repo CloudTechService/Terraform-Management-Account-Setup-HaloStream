c1a3ce58433994986dccdc9127f4378a8bc3df74
sakshiad <sakshi@cloudtechservice.com>
2024-07-05 18:04:45 +0545

    Add module for aft creation

---
+++ variables.tf
@@ -0,0 +1,41 @@
+variable "ct_management_account_id" {
+  type    = string
+  default = "551804445178"
+
+}
+
+variable "log_archive_account_id" {
+  type    = string
+  default = "058264307457"
+
+}
+
+variable "audit_account_id" {
+  type    = string
+  default = "905418040886"
+
+}
+
+variable "aft_management_account_id" {
+  type    = string
+  default = ""
+
+}
+
+variable "ct_home_region" {
+  type    = string
+  default = "ap-south-1"
+
+}
+
+variable "tf_backend_secondary_region" {
+
+  type    = string
+  default = "us-east-2"
+}
+
+variable "github_username" {
+  type    = string
+  default = "CloudTechService"
+
+}
